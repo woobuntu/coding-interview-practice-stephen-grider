fd6479d4d29daab17835f1415089f6d7cd659025
in uh, lee <boutime2017@gmail.com>
2021-08-15 19:17:10 +0900

    integer reversal 도전

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversedValue = parseInt(n.toString().split("").reverse().join(""));
+  return n >= 0 ? reversedValue : -1 * reversedValue;
+}
 
 module.exports = reverseInt;
