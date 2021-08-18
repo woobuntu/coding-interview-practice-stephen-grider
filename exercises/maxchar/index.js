fffe331f850dd02643d9cf54353428cb5bf1d5e5
in uh, lee <boutime2017@gmail.com>
2021-08-18 10:17:32 +0900

    max chars 도전

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const hashOfInputString = str
+    .split("")
+    .reduce(
+      (hash, char) => (
+        hash.hasOwnProperty(char) ? hash[char]++ : (hash[char] = 1), hash
+      ),
+      {}
+    );
+  return Object.entries(hashOfInputString).sort(
+    ([keyOfFormer, countOfFormer], [keyOfLatter, countOfLatter]) =>
+      countOfFormer > countOfLatter ? -1 : 1
+  )[0][0];
+}
 
 module.exports = maxChar;
